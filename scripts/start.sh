9947bc927f189fd8bf129f0a3ccd27543b88c520
r0b0tb4c0n <aeon@robotbacon.net>
2026-01-29 00:47:35 -0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home/node/app
 npm install
+echo
 echo "updating class data"
 python3 /home/node/scripts/data.py
 exec /home/node/app/node_modules/.bin/pm2-runtime ecosystem.config.js
